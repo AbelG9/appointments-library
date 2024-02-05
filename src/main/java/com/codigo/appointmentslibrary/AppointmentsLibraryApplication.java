832ab563dda17956ca9465054bb730a71d0a4020
Abel Guevara <55357845+AbelG9@users.noreply.github.com>
2024-02-04 22:23:36 -0500

    converted project to library

--- src/main/java/com/codigo/appointmentslibrary/AppointmentsLibraryApplication.java
+++ src/main/java/com/codigo/appointmentslibrary/AppointmentsLibraryApplication.java
@@ -3,7 +3,6 @@ package com.codigo.appointmentslibrary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class AppointmentsLibraryApplication {
 
 	public static void main(String[] args) {
